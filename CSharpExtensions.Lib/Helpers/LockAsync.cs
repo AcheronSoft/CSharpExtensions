ddf64d1459131e0dc9a2ab566f698adb61bb6b64
Сергей Сорокин <arcana.magic@outlook.com>
2025-04-07 19:01:13 +0300

    add LockAsync.cs to helpers

---
+++ CSharpExtensions.Lib/Helpers/LockAsync.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpExtensions.Lib.Helpers;
+
+public class LockAsync
+{
+    private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
+    public async Task Lock(Func<Task> func)
+    {
+        await _semaphore.WaitAsync();
+
+        try
+        {
+            await func();
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async Task<T> Lock<T>(Func<Task<T>> func)
+    {
+        await _semaphore.WaitAsync();
+
+        try
+        {
+            return await func();
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
